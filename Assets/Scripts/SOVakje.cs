937cd924b41f53c514c3a941acdf460ce5b3702a
Finn Saal <finn5272@gmail.com>
2023-01-30 13:46:34 +0100

    Added player, table and first script files

---
+++ Assets/Scripts/SOVakje.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOVakje", menuName = "ScriptableObjects/vakje", order = 1)]
+public class SOVakje : ScriptableObject
+{
+   public GameObject vakjePrefab;
+}
