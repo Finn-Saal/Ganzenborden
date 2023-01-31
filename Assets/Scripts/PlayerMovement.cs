29f0495efe44924bb61347eaec94e253d96b469f
Finn Saal <finn5272@gmail.com>
2023-01-31 22:50:13 +0100

    Basic turn functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using System.Dynamic;
 
 public class PlayerMovement : MonoBehaviour
-{
-    public int[] curLoc = {1,1,1,1}; 
-    public static int playNum = 0; //defines the player that rolls dice starting with player 0
+{ 
+    public int playNum = 0; //defines the player that rolls dice starting with player 0
     public GameObject board;
     public Vector3 positionToGo;
     public int playerMax = 3;
+    public int curLoc = 1;
     
     // Start is called before the first frame update
     void Start()
@@ -22,7 +22,7 @@ public class PlayerMovement : MonoBehaviour
         //Debug.Log(board.transform.Find("Vakje " + curLoc).position);
         if(DiceCheckZoneScript.diceStat == true)
         {
-            curLoc[playNum] += DiceNumberTextScript.diceNumber;
+            curLoc += DiceNumberTextScript.diceNumber;
             DiceCheckZoneScript.diceStat = false;
             if(playNum <= playerMax)
             {
@@ -35,12 +35,12 @@ public class PlayerMovement : MonoBehaviour
         }
         
         //makes sure tile number cant go over 63 and moves correct player to new position
-        if(curLoc[playNum] <=63){
+        if(curLoc <= 63){
         FindPosition();
         transform.GetChild(playNum).position = positionToGo;
         }
         else{
-            curLoc[playNum] = 63;
+            curLoc = 63;
         }
     }
 
@@ -52,6 +52,6 @@ public class PlayerMovement : MonoBehaviour
     //Defines position of player
     void FindPosition()
     {
-        positionToGo = board.transform.Find("Vakje " + curLoc[playNum]).position; 
+        positionToGo = board.transform.Find("Vakje " + curLoc).position; 
     }   
 }
