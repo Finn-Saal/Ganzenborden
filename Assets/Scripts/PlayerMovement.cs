3b9677bf28fa8f9aaf1aec3a171cc7e018320fe2
sytze-brstr <boerstra.sytze@gmail.com>
2023-01-31 22:30:10 +0100

    Update playermovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Dynamic;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int curLoc; 
-
+    public int[] curLoc = {1,1,1,1}; 
+    public static int playNum = 0; //defines the player that rolls dice starting with player 0
     public GameObject board;
     public Vector3 positionToGo;
+    public int playerMax = 3;
     
     // Start is called before the first frame update
     void Start()
     {
-        curLoc = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(board.transform.Find("Vakje " + curLoc).position);
-
         if(DiceCheckZoneScript.diceStat == true)
         {
-            curLoc += DiceNumberTextScript.diceNumber;
+            curLoc[playNum] += DiceNumberTextScript.diceNumber;
             DiceCheckZoneScript.diceStat = false;
+            if(playNum <= playerMax)
+            {
+                playNum++;
+            }
+            else
+            {
+                playNum = 0;
+            }
         }
-        if(curLoc <=63){
+        
+        //makes sure tile number cant go over 63 and moves correct player to new position
+        if(curLoc[playNum] <=63){
         FindPosition();
-        transform.GetChild(1).position = positionToGo;
+        transform.GetChild(playNum).position = positionToGo;
         }
         else{
-            curLoc = 63;
+            curLoc[playNum] = 63;
         }
+    }
+
+    void PlayerTurn()
+    {
+        
     }
 
     //Defines position of player
     void FindPosition()
     {
-        
-        positionToGo = board.transform.Find("Vakje " + curLoc).position; 
-        
-    }
-
-     
+        positionToGo = board.transform.Find("Vakje " + curLoc[playNum]).position; 
+    }   
 }
