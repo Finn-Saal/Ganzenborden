6c0a89e2f78a38290591ef9060deb4c6bc41e255
Finn Saal <finn5272@gmail.com>
2023-01-31 21:03:29 +0100

    Main menu restarted

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void PlayGame ()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+            SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex + 1);
+    }    
 }
