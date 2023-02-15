61cbc865d4f8f1735d4246cf60635a6a93661057
Finn Saal <finn5272@gmail.com>
2023-02-15 22:57:34 +0100

    Players move
    Cameras dont

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class CameraSwitch : MonoBehaviour
+public class CameraSwitch : NetworkBehaviour
 {
     public GameObject cameraMain;
     public GameObject cameraA;
